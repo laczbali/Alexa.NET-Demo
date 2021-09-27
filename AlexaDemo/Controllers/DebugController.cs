fe43d1d906e3dc34fcae2968586ff3a6831f3823
Balazs Laczko <laczbali@gmail.com>
2021-09-27 19:14:28 +0200

    - Set up project publishing for IIS hosting - Added debug controller

---
+++ AlexaDemo/Controllers/DebugController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlexaDemo.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class DebugController : ControllerBase
+	{
+		[HttpGet]
+		public string TestMethod()
+		{
+			return "Hello There";
+		}
+	}
+}
